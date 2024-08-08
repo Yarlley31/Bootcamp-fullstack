f4168db1f995196ffc6d8f126bc97237f05918af
Yarlley Fernandes <147113204+Yarlley31@users.noreply.github.com>
2024-08-07 21:22:00 -0300

    convenções

--- fundamentos/models/Pessoa.cs
+++ fundamentos/models/Pessoa.cs
@@ -10,6 +10,7 @@ namespace fundamentos.models
     {
         public string? Nome { get; set; }
         public int Idade { get; set; }
+        public string NomeRepresentateLegalDaPessoaFísica { get; set; }
         public void Apresentar()
         {
             Console.WriteLine($"Olá, meu nome é {Nome},\ne tenho {Idade} anos.");
