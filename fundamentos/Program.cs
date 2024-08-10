79bc33a3c5227d9091c3dbfe1889f496a13e4de5
Yarlley Fernandes <147113204+Yarlley31@users.noreply.github.com>
2024-08-10 20:25:30 -0300

    operadoes aritméticos

--- fundamentos/Program.cs
+++ fundamentos/Program.cs
@@ -1,5 +1,26 @@
 ﻿using fundamentos.models;
 
+
+
+Calculadora calc = new Calculadora();
+
+// calc.Somar(2, 5);
+// calc.Subtrair(60, 53);
+// calc.Multiplicar(7, 4);
+// calc.Divisão(120, 60);
+// calc.Potencia(3,3);
+// calc.Seno(30);
+// calc.Coseno(30);
+// calc.Tangente(30);
+
+calc.RaizQuadrada(9);
+
+
+
+
+
+
+
 // Pessoa Pessoa1 = new Pessoa();
 
 // Pessoa1.Nome = "Yarlley";
@@ -7,30 +28,26 @@
 // Pessoa1.Apresentar(); 
 
 
-Console.WriteLine("Digite uma letra");
-#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
-string letra = Console.ReadLine();
-#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
 
-switch (letra)
-{
-    case "a":
-    case "e":
-    case "i":
-    case "o":
-    case "u":
-        Console.WriteLine("É vogal");
-        break;
+// Console.WriteLine("Digite uma letra");
+// #pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
+// string letra = Console.ReadLine();
+// #pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
 
-    default:
-        Console.WriteLine("Não é vogal");
-        break;
-}
+// switch (letra)
+// {
+//     case "a":
+//     case "e":
+//     case "i":
+//     case "o":
+//     case "u":
+//         Console.WriteLine("É vogal");
+//         break;
 
-
-
-
-
+//     default:
+//         Console.WriteLine("Não é vogal");
+//         break;
+// }
 
 
 
--- fundamentos/models/Calculadora.cs
+++ fundamentos/models/Calculadora.cs
@@ -9,7 +9,55 @@ namespace fundamentos.models
     {
         public void Somar(int x, int y)
         {
-            Console.WriteLine($"{x} + {y} = {x +y}");
+            Console.WriteLine($"{x} + {y} = {x + y}");
+        }
+
+        public void Subtrair(int x, int y)
+        {
+            Console.WriteLine($"{x} - {y} = {x - y}");
+        }
+
+        public void Multiplicar(int x, int y)
+        {
+            Console.WriteLine($"{x} x {y} = {x * y}");
+        }
+
+        public void Divisão(int x, int y)
+        {
+            Console.WriteLine($"{x} / {y} = {x / y}");
+        }
+
+        public void Potencia(int x , int y)
+        {
+            double pot = Math.Pow(x, y);
+            Console.WriteLine($"{x}^{y} = {pot}");
+        }
+
+        public void Seno(double angulo)
+        {
+            double radiano = angulo * Math.PI / 180;
+            double seno = Math.Sin(radiano);
+            Console.WriteLine($"Seno de {angulo}° = {Math.Round(seno, 3)}");
+        }
+
+        public void Coseno(double angulo)
+        {
+            double radiano = angulo * Math.PI / 180;
+            double coseno = Math.Cos(radiano);
+            Console.WriteLine($"O coseno de {angulo}° é {Math.Round(coseno, 3)}");
+        }
+
+        public void Tangente(double angulo)
+        {
+            double radiano = angulo * Math.PI / 180;
+            double tangente = Math.Tan(radiano);
+            Console.WriteLine($"A tangente de {angulo}° é {Math.Round(tangente, 3)}");
+        }
+
+        public void RaizQuadrada(double x)
+        {
+            double raiz = Math.Sqrt(x);
+            Console.WriteLine($"Raiz quadrada de {x} é {raiz}");
         }
     }
 }
